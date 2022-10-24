3bd8c117e74bb1e9a68ac7413634235ed7bbf735
valen <valentin.lluta@uab.edu.bo>
2022-10-24 14:30:10 -0400

    ejercicio clase 24 de octubre (Cristian F. Chipana/Cristian Martinez A./Valentin Lluta)

---
+++ 24Octubre.py
@@ -0,0 +1,11 @@
+limite = int(input('Ingrese un numero: '))
+lista = []
+for i in range(1, limite+1):
+    lista.append(f"{i}")
+    print(" ".join(lista))
+
+# Cristian F. Chipana
+# Cristian Martinez Alejandro
+# Valentin Lluta Choque
+
+# fila 3 - columna 2
