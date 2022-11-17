aad858c82945f9a03cb171b1ec0d764a03fc237a
LideOP <95317741+LideOP@users.noreply.github.com>
2022-11-17 14:51:21 -0400

    Subiendo

--- Octubre/17-11-22 Edil Zapata/Principal.py
+++ Octubre/17-11-22 Edil Zapata/Principal.py
@@ -1,10 +1,9 @@
 from Estudiante import *
 from Docente import *
-class Universidad:
-    def __init__(self):
-        lista=[]
-    
 
 estudiante=Estudiante("Nestor","Caurara",17)
-#estudiante.Docente("Valentin","porde",34,"Ingeniero")
+print("-------------------------------")
+docente=Docente("Valentin","porde",34,"Ingeniero")
+
 print(estudiante)
+print(docente)
